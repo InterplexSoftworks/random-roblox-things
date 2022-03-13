1b052aaebf4879dcbf6a5ffc6cdafe09d36c67cc
dalk <admin@thecool.link>
2022-03-12 20:15:15 -0600

    fixed it

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const randomNumberGenerator = require('./rng.js');
-const id = randomNumberGenerator(1000000,3000000000)
-
-const RoID = `https://www.roblox.com/users/${id}/profile`
-const RoGame = `https://www.roblox.com/games/${id}/RobloxRandoms`
-const RoCatalog = `https://www.roblox.com/catalog/${id}/RobloxRandoms`
 
 exports.RobloxID = function() {
+    // Oh hey, friend! Seems you forgot to do RobloxID(), and did RobloxID! Make sure to fix it!
+    const RoID = `https://www.roblox.com/users/` + randomNumberGenerator(1000000,3000000000) + `/profile`
     return RoID
 }
 exports.RobloxGame = function() {
+    // Oh hey, friend! Seems you forgot to do RobloxGame(), and did RobloxGame! Make sure to fix it!
+    const RoGame = `https://www.roblox.com/games/` + randomNumberGenerator(1000000,3000000000) + `/RobloxRandoms`
     return RoGame
 }
 exports.RobloxCatalog = function() {
+    // Oh hey, friend! Seems you forgot to do RobloxCatalog(), and did RobloxCatalog! Make sure to fix it!
+    const RoCatalog = `https://www.roblox.com/catalog/` + randomNumberGenerator(1000000,3000000000) + `/RobloxRandoms`
     return RoCatalog
 }
